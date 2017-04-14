cf5fbbf2e24167204d5a23ac01bc24c238abf8b5
Christopher <christopher.benz@epfl.ch>
2017-04-14 11:43:43 +0200

    Put server contact in background thread

--- SmartMouse/app/src/main/java/com/lesbobets/smartmouse/MainActivity.java
+++ SmartMouse/app/src/main/java/com/lesbobets/smartmouse/MainActivity.java
@@ -6,13 +6,9 @@ import android.support.v4.view.VelocityTrackerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
-import android.widget.Toast;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private View v;
 
     private ServerContacter mServerContacter;
+    private AsyncTask<Double[], Void, Void> mAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +32,38 @@ public class MainActivity extends AppCompatActivity {
 
         v.setOnTouchListener(mOnTouchListener);
 
+//        mAsyncTask = new SendAsyncTask();
         initClient();
     }
 
     private void initClient() {
-        try {
-            mServerContacter = new ServerContacter();
-            Log.d(TAG, "Initialized Client successfully");
-            Toast.makeText(this, "Initialized Client successfully", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d(TAG, "initClient: Failed to initialize");
-        }
+        mServerContacter = new ServerContacter();
+//        Toast.makeText(this, "Initialized Client successfully", Toast.LENGTH_SHORT).show();
     }
+
+//private class SendAsyncTask extends AsyncTask<Double[], Void, Void> {
+//
+//    private ServerContacter serverContacter;
+//
+//    public SendAsyncTask() {
+//        try {
+//            serverContacter = new ServerContacter();
+//            Log.d(TAG, "Initialized Client successfully");
+//            Toast.makeText(MainActivity.this, "Initialized Client successfully", Toast.LENGTH_SHORT).show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Log.d(TAG, "initClient: Failed to initialize");
+//        }
+//    }
+//
+//    @Override
+//    protected Void doInBackground(Double[]... doubles) {
+//        Double[] dd = doubles[0];
+//        serverContacter.send(dd);
+//        return null;
+//    }
+//
+//}
 
     private View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
         @Override
@@ -75,17 +91,11 @@ public class MainActivity extends AppCompatActivity {
 
                     Log.d(TAG, " Velocities: " + xVelocity + "\t\t" + yVelocity);
 
-                    new AsyncTask<Pair<Float, Float>, Void, Void>() {
-
-                        @Override
-                        protected Void doInBackground(Pair<Float, Float>... vs) {
-                            if (mServerContacter != null) {
-                                Pair<Float, Float> v = vs[0];
-                                mServerContacter.send(v.first, v.second);
-                            }
-                            return null;
-                        }
-                    }.execute(Pair.create(xVelocity, yVelocity));
+                    final Double[] velocities = new Double[]{Double.valueOf(xVelocity), Double.valueOf(yVelocity)};
+//                    mAsyncTask.execute(velocities);
+                    if (mServerContacter != null) {
+                        mServerContacter.send(velocities);
+                    }
 
                     break;
                 case MotionEvent.ACTION_UP:
--- SmartMouse/app/src/main/java/com/lesbobets/smartmouse/ServerContacter.java
+++ SmartMouse/app/src/main/java/com/lesbobets/smartmouse/ServerContacter.java
@@ -13,25 +13,46 @@ public class ServerContacter {
     private PrintWriter out;
     private String myIp = "192.168.8.107";
 
-    public ServerContacter() throws IOException {
+    public ServerContacter() {
         this("192.168.8.107", 8000);
     }
 
-    public ServerContacter(String ipAddress, int port) throws IOException {
-        clientSocket = new Socket(ipAddress, port);
-        out = new PrintWriter(clientSocket.getOutputStream());
-        Log.d("ServerContacter", out.toString());
-    }
-
-    public void send(final double xVelocity, final double yVelocity) {
-
-        final double[] velocities = new double[]{xVelocity, yVelocity};
+    public ServerContacter(final String ipAddress, final int port) {
+//        clientSocket = new Socket(ipAddress, port);
+//        out = new PrintWriter(clientSocket.getOutputStream());
+//        Log.d("ServerContacter", out.toString());
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                out.println(Arrays.toString(velocities));
-                out.flush();
+                try {
+                    clientSocket = new Socket(ipAddress, port);
+                    out = new PrintWriter(clientSocket.getOutputStream());
+                    Log.d("BLABLA", "run: Initialized Client successfully");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("BLABLA", "run: Initialize Client FAILED");
+                }
+                ;
+            }
+        }).start();
+    }
+
+    public void send(final double xVelocity, final double yVelocity) {
+        final Double[] velocities = new Double[]{xVelocity, yVelocity};
+        send(velocities);
+    }
+
+    public void send(final Double[] velocities) {
+//        out.println(Arrays.toString(velocities));
+//        out.flush();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (clientSocket != null && out != null) {
+                    out.println(Arrays.toString(velocities));
+                    out.flush();
+                }
             }
         }).start();
     }
