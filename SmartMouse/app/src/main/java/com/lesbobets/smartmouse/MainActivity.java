b9a7c11486823391813182e898ac381a06bd61bb
Joachim <joachim.huet@epfl.ch>
2017-04-14 18:09:09 +0200

    All working

--- SmartMouse/app/src/main/java/com/lesbobets/smartmouse/MainActivity.java
+++ SmartMouse/app/src/main/java/com/lesbobets/smartmouse/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 
 public class MainActivity extends AppCompatActivity implements
         GestureDetector.OnGestureListener,
@@ -47,40 +48,12 @@ public class MainActivity extends AppCompatActivity implements
         mDetector = new GestureDetectorCompat(this, this);
         mDetector.setOnDoubleTapListener(this);
 
-//        v = findViewById(R.id.touch_view);
-//        mDetector.setOnTouchListener(mOnTouchListener);
-//        mAsyncTask = new SendAsyncTask();
         initClient();
     }
 
     private void initClient() {
         mServerContacter = new ServerContacter();
-//        Toast.makeText(this, "Initialized Client successfully", Toast.LENGTH_SHORT).show();
     }
-
-//private class SendAsyncTask extends AsyncTask<Double[], Void, Void> {
-//
-//    private ServerContacter serverContacter;
-//
-//    public SendAsyncTask() {
-//        try {
-//            serverContacter = new ServerContacter();
-//            Log.d(TAG, "Initialized Client successfully");
-//            Toast.makeText(MainActivity.this, "Initialized Client successfully", Toast.LENGTH_SHORT).show();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            Log.d(TAG, "initClient: Failed to initialize");
-//        }
-//    }
-//
-//    @Override
-//    protected Void doInBackground(Double[]... doubles) {
-//        Double[] dd = doubles[0];
-//        serverContacter.sendVelocities(dd);
-//        return null;
-//    }
-//
-//}
 
 //    private View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
 //
@@ -131,6 +104,41 @@ public class MainActivity extends AppCompatActivity implements
         this.multiTouchListener.onTouchEvent(event);
         this.mDetector.onTouchEvent(event);
 
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (mVelocityTracker == null) {
+                    mVelocityTracker = VelocityTracker.obtain();
+                } else {
+                    mVelocityTracker.clear();
+                }
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker.addMovement(event);
+                mVelocityTracker.computeCurrentVelocity(1000);
+                float xVelocity = VelocityTrackerCompat.getXVelocity(mVelocityTracker, pointerId);
+                float yVelocity = VelocityTrackerCompat.getYVelocity(mVelocityTracker, pointerId);
+
+                Log.d(TAG, " Velocities: " + xVelocity + "\t\t" + yVelocity);
+
+                final Double[] velocities = new Double[]{Double.valueOf(xVelocity), Double.valueOf(yVelocity)};
+//                    mAsyncTask.execute(velocities);
+                if (mServerContacter != null) {
+                    mServerContacter.sendVelocities(velocities);
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mVelocityTracker.recycle();
+                mVelocityTracker = null;
+                break;
+        }
+
         // Be sure to call the superclass implementation
         return super.onTouchEvent(event);
     }
@@ -155,43 +163,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onDown(MotionEvent event) {
-        int index = event.getActionIndex();
-        int action = event.getActionMasked();
-        int pointerId = event.getPointerId(index);
-
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                if (mVelocityTracker == null) {
-                    mVelocityTracker = VelocityTracker.obtain();
-                } else {
-                    mVelocityTracker.clear();
-                }
-                mVelocityTracker.addMovement(event);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                mVelocityTracker.addMovement(event);
-                mVelocityTracker.computeCurrentVelocity(1000);
-                float xVelocity = VelocityTrackerCompat.getXVelocity(mVelocityTracker,
-                        pointerId);
-                float yVelocity = VelocityTrackerCompat.getYVelocity(mVelocityTracker,
-                        pointerId);
-
-                Log.d(TAG, " Velocities: " + xVelocity + "\t\t" + yVelocity);
-
-                final Double[] velocities = new Double[]{Double.valueOf(xVelocity), Double.valueOf(yVelocity)};
-//                    mAsyncTask.execute(velocities);
-                if (mServerContacter != null) {
-                    mServerContacter.sendVelocities(velocities);
-                }
-
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                mVelocityTracker.recycle();
-                mVelocityTracker = null;
-                break;
-        }
-        return true;
+        return false;
     }
 
     @Override
